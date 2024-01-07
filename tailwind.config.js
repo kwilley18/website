9993b9a1739d4dfc9301a27e0d583d5403e8010b
Kristian Willey <kristianwilley@gmail.com>
2024-01-06 23:09:54 -0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/style.css/*.{html,js}"],
-  theme: {
-    extend: {},
+  content: ["/*.html"],
+  theme: { 
+    extend: {
+      colors: {
+        'primary': '#3b7977',
+        'secondary': '#57a99a',
+      },
+      height: {
+        '128': '20rem',
+        'half': '50vh',
+        '3/4': '75vh'
+      },
+    },
+    //extend: {},
   },
   plugins: [],
 }
